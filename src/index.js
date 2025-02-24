a72a417ceb3de46de52d8ac928605ec93815f7a1
Ethan <khoitranec@gmail.com>
2025-02-25 01:39:00 +0700

    clear un-use space

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import './style.css';
 import { updateDOM } from './dom';
+
 const input = document.querySelector('input');
 const searchButton = document.getElementById('search');
 const toggleButton = document.getElementById('toggle');
 const apiKey = '57GV7C6SHUYNDBU83BSHUSEGQ';
+const loading = document.getElementById('loading');
 
 searchButton.addEventListener('click', () => {
   const infoDiv = document.getElementById('info');
-
   const location = document.getElementById('location');
   const time = document.getElementById('time');
   const temperature = document.getElementById('temperature');
   const condition = document.getElementById('condition');
 
   const inputLocation = input.value;
+
+  loading.style.display = 'block';
+  infoDiv.style.display = 'none';
 
   async function weatherInfo() {
     try {
@@ -26,6 +30,8 @@ searchButton.addEventListener('click', () => {
 
       // update DOM
       updateDOM(location, time, temperature, condition, infoDiv, data);
+
+      loading.style.display = 'none';
     } catch (error) {
       console.error('Error fetching weather:', error.message);
     }
@@ -34,3 +40,5 @@ searchButton.addEventListener('click', () => {
 
   input.value = '';
 });
+
+// WORKING ON THE TOGGLE BUTTON
